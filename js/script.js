8c4580c3c6f7b5ed0c0ed8c157cc3adf7c8e1e68
cwen0008 <cwen0008@student.monash.edu>
2024-05-23 21:37:39 +1000

    js function

--- js/script.js
+++ js/script.js
@@ -1,1 +1,37 @@
-                  
+$(document).ready(function() {
+    $('.book-now').click(function(event) {
+        event.preventDefault();
+        let days = prompt("Enter the number of days you want to stay:");
+
+        if (days !== null && days !== "") {
+            days = parseInt(days);
+            let pricePerDay = 99;
+            let totalPrice = days * pricePerDay;
+
+            if (days > 7) {
+                totalPrice = totalPrice * 0.75; // 25% off
+            }
+
+            alert("The total price for " + days + " days is around: $" + totalPrice.toFixed(2));
+        }
+    });
+});
+
+$(document).ready(function() {
+    $('.book-now1').click(function(event) {
+        event.preventDefault();
+        let days = prompt("Enter the number of days you want to stay:");
+
+        if (days !== null && days !== "") {
+            days = parseInt(days);
+            let pricePerDay = 139;
+            let totalPrice = days * pricePerDay;
+
+            if (days > 7) {
+                totalPrice = totalPrice * 0.75; // 25% off
+            }
+
+            alert("The total price for " + days + " days is around: $" + totalPrice.toFixed(2));
+        }
+    });
+});
